49e0494bd69332a4e53d4c3234381cc1822cc2d2
Piyushraj Bista <piyushrajbista@outlook.com>
2023-03-31 15:09:45 +0545

    feat: added Header and SelectOptions as child Component

---
+++ src/pages/landing/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./shared/Header";
+import SelectOptions from "./SelectOptions";
+function Landing() {
+  return (
+    <>
+      <Header />
+      <SelectOptions />
+    </>
+  );
+}
+
+export default Landing;
