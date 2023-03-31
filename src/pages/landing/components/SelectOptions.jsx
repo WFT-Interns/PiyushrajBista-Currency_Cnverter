94371fa0f95c7cd6bb6d832341a8e563168f618f
Piyushraj Bista <piyushrajbista@outlook.com>
2023-03-31 15:08:37 +0545

    feat: called axios instance to call API to display data in MenuItem

--- src/pages/landing/components/SelectOptions.jsx
+++ src/pages/landing/components/SelectOptions.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FormControl,
   InputLabel,
@@ -6,14 +6,34 @@ import {
   Select,
   Button,
 } from "@mui/material";
+import { useQuery } from "react-query";
+import { instance } from "../../../config/axios.config";
 
 function SelectOptions() {
+  const [currencyCode, setCurrencyCode] = useState("");
+
+  const { isLoading, error, data } = useQuery("currentData", () => {
+    return instance.get("/latest/USD").then((res) => {
+      return res.data;
+    });
+  });
+
   return (
     <>
-      <FormControl sx={{ width: "200px", marginRight: "80px" }}>
+      <FormControl sx={{ width: "200px", marginTop: "80px" }}>
         <InputLabel id="select-base-currency">Select Base Currency</InputLabel>
-        <Select labelId="select-autowidth-label" label="Select Base Cuurency">
-          <MenuItem></MenuItem>
+        <Select
+          labelId="select-autowidth-label"
+          value={currencyCode}
+          label="Select Base Currency"
+          onChange={(event) => setCurrencyCode(event.target.value)}
+        >
+          {data &&
+            Object.keys(data.conversion_rates).map((currency) => (
+              <MenuItem key={currency} value={currency}>
+                {currency}
+              </MenuItem>
+            ))}
         </Select>
         <Button sx={{ marginTop: "20px" }} variant="contained">
           Submit
