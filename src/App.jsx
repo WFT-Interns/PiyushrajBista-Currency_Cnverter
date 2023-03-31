39099340d77e42b07f822823de3dfef272bb7da7
Piyushraj Bista <piyushrajbista@outlook.com>
2023-03-31 12:06:04 +0545

    feat: Called landing component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Landing from "./pages/landing/components/Landing";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
 }
 
 export default App;
