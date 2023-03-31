84bafbd275ec84fb86fae31f215276dc7e26c159
Piyushraj Bista <piyushrajbista@outlook.com>
2023-03-31 11:30:06 +0545

    feat: Created Select Form

---
+++ src/pages/landing/components/SelectOptions.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Button,
+} from "@mui/material";
+
+function SelectOptions() {
+  return (
+    <>
+      <FormControl sx={{ width: "200px", marginRight: "80px" }}>
+        <InputLabel id="select-base-currency">Select Base Currency</InputLabel>
+        <Select
+          labelId="select-autowidth-label"
+          label="Select Base Cuurency"
+        ></Select>
+        <Button sx={{ marginTop: "20px" }} variant="contained">
+          Submit
+        </Button>
+      </FormControl>
+    </>
+  );
+}
+
+export default SelectOptions;
